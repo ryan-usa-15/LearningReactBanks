09d23d85a5b26553aeb90c055390935b1ce5d15a
Your Name <you@example.com>
2025-01-30 08:07:58 -0800

    first

---
+++ more.js
@@ -0,0 +1,7 @@
+import {print, log } from "./things";
+
+
+print("printing message")
+log("logging message");
+
+// can't test because compile and run
---
+++ things.js
@@ -0,0 +1,281 @@
+
+
+
+
+
+
+const[first] = ["dog", "lizard", "cat"]
+
+
+console.log(first)
+
+const[,, third] = ["blue", "green", "yellow"]
+
+
+console.log(third)
+
+
+
+
+const called = "Tallac";
+const elevation = 9738;
+
+
+const mountain = {
+    called,
+    elevation, 
+    teach() {
+        console.log(`Mt. ${this.called} is ${this.elevation} feet tall.`)
+    }
+}
+
+console.log(mountain.teach())
+
+
+function directions(...args){
+
+    let [start, ...remaining] = args;
+    let [finish, ...stops] = remaining.reverse()
+
+    console.log(`count: ${args.length}`)
+    console.log(`start in ${start}`)
+    console.log(`destination: ${finish}`)
+    console.log(`stops between: ${stops.length}`)
+
+
+
+}
+
+directions("Truckee", "Tahoe City", "Sunnyside", "Homewood", "Tahoma");
+
+
+
+fetch("https://api.randomuser.me/?nat=US&results=1")
+    .then(res => res.json())
+    .then(json => json.results)
+    .then(console.log)
+    .catch(console.error)
+
+
+const getFakePerson = async function myFunction() {
+    try{
+        let res = await fetch("https://api.randomuser.me/?nat=US&results=1");
+        let {results} = await res.json();
+        console.log(results);
+        console.log("no error");
+    }catch(error){
+        console.error(error)
+    }
+
+};
+
+
+getFakePerson();
+
+
+const getPeople = count =>
+    new Promise((resolves, rejects) =>{
+
+        const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+        const request = new XMLHttpRequest();
+        request.open("GET", api);
+        request.onload = () => 
+            request.status === 200
+                ? resolves(JSON.parse(request.response).results)
+                : rejects(Error(request.statusText));
+        request.onerror = error => rejects(err);
+        request.send();
+
+
+
+    });
+
+
+    getPeople(5)
+    .then(members => console.log(members))
+    .catch(error => console.error(`getPeople failed ${error.message}`));
+
+
+class Vacation {
+
+    constructor (destination, length){
+        this.destination = destination;
+        this.length = length;
+    }
+
+    print(){
+        console.log(`${this.destination} will take ${this.length}`);
+    }
+}
+
+class Expedition extends Vacation{
+
+    constructor(destination, length,gear){
+        super(destination, length)
+        this.gear = gear;
+    }
+
+    print(){
+        super.print();
+        console.log(`Bring your ${this.gear.join(" and your ")}`);
+    }
+
+
+}
+
+const trip = new Expedition("Mt. Whitney", 3, ["sunglasses", "camera", "trail mix"]);
+
+trip.print();
+
+
+export const print=(message) => log(message, new Date())
+
+export const log=(message, timestamp) => 
+    console.log(`${timestamp.toString()}: ${message}`);
+
+
+
+
+
+const createScream = function(logger){
+    return function(message){
+        logger(message.toUpperCase() + "!!!");
+    };
+};
+
+const scream = createScream(message => console.log(message))
+
+scream("functions can be returned by other functions");
+
+
+// add to versus concat with copy
+let list = [{title: "rad red"}]
+const addColor = (title, array) => array.concat({title})
+const newlist = addColor("glam green", list)
+console.log(newlist.length);
+console.log(list.length)
+
+// same thing with spread operator 
+const addColorBrief = (title, list) => [...list, {title}]
+addColorBrief("royal purple", list)[1].title
+
+// add to actual list not immutable
+list.push({title:"mellow yellow"});
+console.log(list.length)
+
+let color_lawn = {
+    title: "lawn",
+    color: "#00FF00",
+    rating: 0
+}
+
+const rateColor = function(color, rating){
+    return Object.assign({}, color, {rating: rating})
+};
+
+console.log(rateColor(color_lawn, 5).rating)
+console.log(color_lawn.rating)
+
+
+// ch 3. change one object 
+let places = [
+    {name: "Yorktown"},
+    {name: "Stratford"},
+    {name: "Wakefield"}
+]
+
+const editName = (oldName, name, arr)=> arr.map(item => (item.name === oldName ? {...item, name}: item))
+
+let updatedPlaces = editName("Stratford", "HB Woodlawn", places);
+
+
+
+console.log(updatedPlaces)
+
+
+
+
+// ch. 3 - object to array
+const schools = {
+    Yorktown: 10, 
+    "Washington & Liberty": 2, 
+    Wakefield: 5
+}
+const schoolArray = Object.keys(schools).map(key => ({ 
+    name: key, 
+    wins: schools[key]
+}))
+
+console.log(schoolArray)
+
+
+
+// calculate max value with reduce 
+
+const ages = [3, 33, 66, 77, 99, 2, 4, 11, 56]
+
+const max = ages.reduce((max, value) => (value> max? value : max), 0)
+
+console.log(max);
+
+
+/// array to a hash object 
+const colors=[
+    {
+        id: "xekare",
+        title: "rad red",
+        rating: 3
+    },
+    {
+        id: "jbwsof",
+        title: "big blue",
+        rating: 2
+    }
+
+]
+
+const hashColors = colors.reduce(
+    (hash, {id, title, rating}) => 
+        {
+            hash[id] = {title, rating};
+            return hash
+        }, 
+    {});
+
+console.log(hashColors)
+
+///////////////////////////////////////////////////////
+
+
+// #1 nested value retrieval 
+const dan = {
+    type: "person",
+    data: {
+        gender: "male",
+        info: {
+            id: 22, 
+            fullname: {
+                first: "Dan",
+                last: "Deacon"
+            }
+        }
+    }
+}
+
+const deepPick = (fields, object = {}) => {
+    const [first, ...remaining] = fields.split(".");
+    return remaining.length
+        ? deepPick(remaining.join("."), object[first]) 
+        : object[first];
+};
+
+deepPick("type", dan)
+deepPick("data.info.fullname.first", dan);
+
+
+
+
+
+
+
+
