e512fb6e2fc857cb97a9a647b18addcf3e8d9355
Your Name <you@example.com>
2025-01-30 09:00:22 -0800

    2

--- things.js
+++ things.js
@@ -273,7 +273,16 @@ deepPick("type", dan)
 deepPick("data.info.fullname.first", dan);
 
 
+//////// #2 countdown with delay - recursion, higher order function
+const countdown = (value, fn, delay = 1000) => {
+    fn(value);
+    return value > 0
+        ? setTimeout(() => countdown(value - 1, fn, delay), delay)
+        :value;
+};
 
+const log = value => console.log(value);
+countdown(10, log);
 
 
 
